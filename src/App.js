35384d9284fd82983f0e6a4602520c16568ed314
John R. Estes <John.R.Estes@icloud.com>
2020-10-08 19:25:19 -0400

    setup jquery

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
 import React from 'react';
 import './App.css';
+import $ from 'jquery';
+
+function setHeight(){
+  var width = $( "iframe" ).width();
+  var height = (width * .5625);
+  $( "iframe" ).css("height", `${height}`);
+
+}
 
 function App() {
   return (
@@ -10,13 +18,15 @@ function App() {
         </div>
       </div>
       <div className="row">
-        <div className="col-9">
-          <iframe id="ytplayer" type="text/html" width="640" height="360"
-            src="https://www.youtube.com/embed/axCcDUbeC2Y?autoplay=1&origin=http://example.com"
-            frameborder="0">  
-          </iframe>
+        <div className="col-lg-9 col-sm-12">
+          <div  id="playerDiv">
+            <iframe id="ytplayer" type="text/html" 
+              src="https://www.youtube.com/embed/axCcDUbeC2Y?autoplay=1&origin=http://example.com"
+              frameborder="0" > 
+            </iframe>
+          </div>
         </div>
-        <div className="col-3">
+        <div className="col-lg-3 col-sm-12" id="relatedVideos">
           <p>Related Videos</p>
         </div>
       </div>
