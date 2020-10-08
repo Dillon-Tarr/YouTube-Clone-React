511e740ddf49f11ced2d9f1336a1eed834da3fc0
John R. Estes <John.R.Estes@icloud.com>
2020-10-08 18:47:18 -0400

    setup React for bootstrap

--- src/App.js
+++ src/App.js
@@ -3,21 +3,23 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container-fluid">
+      <div className="row">
+        <div className="col-12">
+          <input></input>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-9">
+          <iframe id="ytplayer" type="text/html" width="640" height="360"
+            src="https://www.youtube.com/embed/axCcDUbeC2Y?autoplay=1&origin=http://example.com"
+            frameborder="0">  
+          </iframe>
+        </div>
+        <div className="col-3">
+          <p>Related Videos</p>
+        </div>
+      </div>
     </div>
   );
 }
